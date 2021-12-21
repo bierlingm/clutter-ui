d0ca51b13d51e9b9bb0328d59c2f871b25be77f6
bierlingm <moritz.bierling@bigsun.de>
2021-12-21 18:30:41 +0100

    Remove margin in styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	export let name: string;
+	import Page from "./stories/Page.svelte"
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<body>
+	<main>
+		<Page/>
+	</main>
+</body>
 
 <style>
 	main {
@@ -13,13 +14,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
